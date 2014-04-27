7946c457f8d8849229b133c77ed09abbb7b455c8
Kurt Sang Hyun Lee <kurt@vingle.net>
2014-04-27 22:43:01 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madeleine::Application.config.secret_token = '21113f2c302b2535e6ef0f81ece3300f5eeaa3831df8c2eba9ab5f14ab8722784a691466e8fdfa9323b28e283fa167b95bf817a3dda20283b8a561cafa307927'
